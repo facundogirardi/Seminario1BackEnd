c3f8239d91493b3395ae4d39848fbefcd190bcc7
unknown <facundogirardi@gmail.com>
2020-11-11 22:51:16 -0300

    Deleteo usuario

--- routes/api/user.route.js
+++ routes/api/user.route.js
@@ -17,8 +17,10 @@ router.post('/login/', UserController.loginUser)
 router.get('/tusuarios', UserController.getUsers)
 /*router.get('/',Authorization, UserController.getUsers)*/ /*Temporal le quito seguridad*/
 router.post('/userByMail', Authorization, UserController.getUsersByMail)
-router.put('/actualizacion', Authorization, UserController.updateUser)
-router.delete('/deleteo', Authorization, UserController.removeUser)
+router.put('/actualizacion', UserController.updateUser)
+router.delete('/deleteo', UserController.removeUser)
+/*router.put('/actualizacion', Authorization, UserController.updateUser)*/
+/*router.delete('/deleteo', Authorization, UserController.removeUser)*/
 router.post('/guardarImgUser',UserController.guardarImagenUser)
 router.post('/uploadImg',UploadController.uploadFilesImgUser);
 router.post('/imgUserByMail',Authorization,UserController.getImagenUserByMail)
